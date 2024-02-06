a0590349c544224a524bb6d7e3a1beaead1aa811
Kolcman <m.kolcman@yandex.ru>
2024-02-06 11:59:02 +0300

    Lesson2:task2

--- Program.cs
+++ Program.cs
@@ -4,11 +4,28 @@
 // Console.WriteLine(number);
 // Console.WriteLine(number2);
 
-Console.Write("Введите первое число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int number2 = Convert.ToInt32(Console.ReadLine());
-if (number1 == (number2 * number2))
+// Console.Write("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// if (number1 == (number2 * number2))
+// {
+//     Console.WriteLine("Да");
+// } else Console.WriteLine("Нет");
+
+System.Console.WriteLine("Введите первое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+// int i = - N;
+// while(i <= N)
+// {
+//     System.Console.Write(i + ",");
+//     i++;
+// }
+
+
+
+for (int i = -N; i <= N; i++)
 {
-    Console.WriteLine("Да");
-} else Console.WriteLine("Нет");
+    System.Console.Write(i + ", ");
+}

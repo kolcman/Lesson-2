1a51ca07dcbc55720e8d079d476357629bcfdbdf
Kolcman <m.kolcman@yandex.ru>
2024-02-06 11:01:09 +0300

    Lesson2:Task1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// int number = int.Parse(Console.ReadLine());
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(number);
+// Console.WriteLine(number2);
+
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == (number2 * number2))
+{
+    Console.WriteLine("Да");
+} else Console.WriteLine("Нет");
